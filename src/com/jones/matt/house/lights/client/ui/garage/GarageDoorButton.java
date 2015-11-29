9c460a2594ca73f49af11628fe1bb41c742c3783
Matt Jones <bigboxer23@gmail.com>
2015-11-28 21:31:36 -0600

    Updating garage button to have sub panel for disablign auto close, displaying time until auto close will happen remove button for garage light, put into new garage sub panel

--- src/com/jones/matt/house/lights/client/ui/garage/GarageDoorButton.java
+++ src/com/jones/matt/house/lights/client/ui/garage/GarageDoorButton.java
@@ -1,15 +1,21 @@
 package com.jones.matt.house.lights.client.ui.garage;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.event.logical.shared.ValueChangeHandler;
+import com.google.gwt.user.client.ui.Image;
 import com.googlecode.mgwt.dom.client.event.tap.TapEvent;
 import com.googlecode.mgwt.dom.client.event.tap.TapHandler;
 import com.googlecode.mgwt.ui.client.widget.button.Button;
+import com.googlecode.mgwt.ui.client.widget.image.ImageHolder;
 import com.googlecode.mgwt.ui.client.widget.panel.flex.FlexPanel;
 import com.googlecode.mgwt.ui.client.widget.panel.flex.FlexPropertyHelper;
 import com.jones.matt.house.lights.client.event.EventBusInstance;
+import com.jones.matt.house.lights.client.event.FireableValueChangeEvent;
 import com.jones.matt.house.lights.client.model.DeviceVO;
 import com.jones.matt.house.lights.client.model.RoomVO;
+import com.jones.matt.house.lights.client.ui.animation.AnimationStack;
 import com.jones.matt.house.lights.client.utility.DefaultRequestBuilder;
 
 /**
@@ -20,7 +26,7 @@ public class GarageDoorButton extends FlexPanel implements TapHandler, ValueChan
 {
 	private Button myGarageButton;
 
-	private WeatherLabel myWeatherLabel;
+	private GarageDetailPanel myGarageDetailPanel;
 
 	public GarageDoorButton()
 	{
@@ -30,8 +36,22 @@ public class GarageDoorButton extends FlexPanel implements TapHandler, ValueChan
 		myGarageButton.addStyleName("garage-button");
 		myGarageButton.addTapHandler(this);
 		add(myGarageButton);
-		myWeatherLabel = new WeatherLabel();
-		add(myWeatherLabel);
+		myGarageDetailPanel = new GarageDetailPanel();
+		Image aRoomDetails = new Image(ImageHolder.get().nextItem());
+		aRoomDetails.addClickHandler(new ClickHandler()
+		{
+			@Override
+			public void onClick(ClickEvent event)
+			{
+				AnimationStack.getInstance().forward(myGarageDetailPanel);
+			}
+		});
+		aRoomDetails.addStyleName("RoomDetails");
+		add(aRoomDetails);
+		WeatherLabel aWeatherLabel = new WeatherLabel();
+		addHandler(aWeatherLabel, ValueChangeEvent.getType());
+		addHandler(myGarageDetailPanel, ValueChangeEvent.getType());
+		add(aWeatherLabel);
 		EventBusInstance.getInstance().addValueChangeHandler(this);
 	}
 
@@ -46,13 +66,14 @@ public class GarageDoorButton extends FlexPanel implements TapHandler, ValueChan
 	{
 		if (theEvent.getValue().getName().equals("Garage"))
 		{
+			myGarageDetailPanel.onRoomUpdate(theEvent.getValue());
 			for (DeviceVO aDevice : theEvent.getValue().getDevices())
 			{
 				if (aDevice.getName().equals("Garage Opener"))
 				{
 					myGarageButton.setText(!aDevice.getDoor() ? "Open Garage" : "Close Garage");
-					myWeatherLabel.update(aDevice);
 				}
+				fireEvent(new FireableValueChangeEvent<>(aDevice));
 			}
 		}
 	}
