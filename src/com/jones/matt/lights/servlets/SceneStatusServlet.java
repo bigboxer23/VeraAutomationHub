9c91740c4605f25f0f32c36f929632ceed0e6490
Matt Jones <bigboxer23@gmail.com>
2016-02-20 16:33:32 -0600

    remove decimal point from weather labels update sceneStatusServlet w/timeout so if/when garage fails, don't hang service update thermostat change panel UI to change state in a more user friendly manner when status interval is hit.

--- src/com/jones/matt/lights/servlets/SceneStatusServlet.java
+++ src/com/jones/matt/lights/servlets/SceneStatusServlet.java
@@ -11,12 +11,17 @@ import com.jones.matt.lights.controllers.vera.VeraDeviceVO;
 import com.jones.matt.lights.controllers.vera.VeraHouseVO;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.params.HttpClientParams;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.net.SocketTimeoutException;
 
 /**
  * Get status from the vera controller for everything in the house
@@ -38,7 +43,10 @@ public class SceneStatusServlet extends AbstractServlet
 	public void process(HttpServletRequest theRequest, HttpServletResponse theResponse) throws ServletException, IOException
 	{
 		theResponse.setContentType("application/json");
-		DefaultHttpClient aHttpClient = new DefaultHttpClient();
+		HttpParams aParams = new BasicHttpParams();
+		HttpConnectionParams.setSoTimeout(aParams, 10000);
+		HttpConnectionParams.setConnectionTimeout(aParams, 10000);
+		DefaultHttpClient aHttpClient = new DefaultHttpClient(aParams);
 		try
 		{
 			HttpResponse aResponse = aHttpClient.execute(new HttpGet(VeraController.kVeraHubUrl + "/data_request?id=sdata"));
@@ -53,7 +61,6 @@ public class SceneStatusServlet extends AbstractServlet
 			}
 			((VeraController)HubContext.getInstance().getControllers().get(VeraController.kControllerEndpoint)).setStatus(aHouseStatus);
 			theResponse.getOutputStream().print(new Gson().toJson(aHouseStatus));
-
 			aResponse = aHttpClient.execute(new HttpGet(GarageController.kGarageURL + "/Status2"));
 			myGarageData = myBuilder.create().fromJson(new String(ByteStreams.toByteArray(aResponse.getEntity().getContent()), Charsets.UTF_8), VeraDeviceVO.class);
 			myGarageData.setName("Garage Opener");
@@ -61,7 +68,7 @@ public class SceneStatusServlet extends AbstractServlet
 		}
 		catch (IOException e)
 		{
-			e.printStackTrace();
+			log("SceneStatusServlet:", e);
 		}
 		theResponse.getOutputStream().flush();
 		theResponse.getOutputStream().close();
