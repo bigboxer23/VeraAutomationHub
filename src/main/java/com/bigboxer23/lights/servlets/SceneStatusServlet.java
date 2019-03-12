282347bac555058eb6323ddf1383a7de7e9584bc
Matt Jones <bigboxer23@gmail.com>
2019-03-11 20:13:06 -0500

    library updates http client closing bug

--- src/main/java/com/bigboxer23/lights/servlets/SceneStatusServlet.java
+++ src/main/java/com/bigboxer23/lights/servlets/SceneStatusServlet.java
@@ -3,10 +3,7 @@ package com.bigboxer23.lights.servlets;
 import com.bigboxer23.lights.HubContext;
 import com.bigboxer23.lights.controllers.elastic.ElasticAnalyticsController;
 import com.bigboxer23.lights.controllers.vera.VeraHouseVO;
-import com.bigboxer23.lights.controllers.vera.VeraSceneVO;
 import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,9 +14,6 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * Get status from the vera controller for everything in the house
  */
@@ -28,8 +22,6 @@ import java.util.Map;
 @EnableAutoConfiguration
 public class SceneStatusServlet extends HubContext
 {
-	private static final String kLevelSetSceneName = System.getProperty("level.set.scene.name", "LevelSet");
-
 	private static final Logger myLogger = LoggerFactory.getLogger(SceneStatusServlet.class);
 
 	private long myLastUpdate = -1;
@@ -44,11 +36,6 @@ public class SceneStatusServlet extends HubContext
 	{
 		myElasticAnalyticsController = theElasticAnalyticsController;
 	}
-
-	/**
-	 * Device id, load level
-	 */
-	private Map<Integer, Integer> mySpecificDimLevels = new HashMap<>();
 
 	@RequestMapping(value = "/SceneStatus", produces = {MediaType.APPLICATION_JSON_VALUE})
 	public String getHouseStatusJson()
@@ -107,43 +94,6 @@ public class SceneStatusServlet extends HubContext
 								.ifPresent(theVeraRoomVO -> theVeraRoomVO.getDevices().add(theDevice));
 					});
 				});
-	}
-
-	private void fillLevels(VeraHouseVO theHouse)
-	{
-			theHouse.getDevices().stream().
-					filter(theDeviceVO -> mySpecificDimLevels.containsKey(theDeviceVO.getId())).
-					forEach(theDeviceVO -> theDeviceVO.setDefinedDim(mySpecificDimLevels.get(theDeviceVO.getId())));
-	}
-
-	/**
-	 * Every minute query the server for scene information which we can gain custom on/off/dim percentage for
-	 * each room's lights.  If a dim is set, or off, then we use that or don't turn that light on with the
-	 * containing room (individual light requests still perform as expected
-	 *
-	 * @param theVO
-	 */
-	private void setupLevels(VeraSceneVO theVO)
-	{
-		if (myLastUpdate < System.currentTimeMillis() - myUpdateInterval)
-		{
-			myLogger.debug("Fetching new levels---------------------");
-			myLastUpdate = System.currentTimeMillis();
-			JsonObject anElement = myVeraController.getSceneInformation(theVO.getId());
-			JsonArray aDevices = anElement.getAsJsonArray("groups").get(0).getAsJsonObject().getAsJsonArray("actions");
-			for (int ai = 0; ai < aDevices.size(); ai++)
-			{
-				JsonObject aDevice = aDevices.get(ai).getAsJsonObject();
-				int aDeviceId = aDevice.get("device").getAsInt();
-				int aDimLevel = aDevice.get("arguments").getAsJsonArray().get(0).getAsJsonObject().get("value").getAsInt();
-				int aPrevious = mySpecificDimLevels.getOrDefault(aDeviceId, -1);
-				if (aPrevious != aDimLevel)
-				{
-					myLogger.error("device: " + aDeviceId + " set from " + aPrevious + " to " + aDimLevel);
-					mySpecificDimLevels.put(aDeviceId, aDimLevel);
-				}
-			}
-		}
 	}
 
 	/**
