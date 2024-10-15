e9d2b400a8a0054496ac7b2f86ba87b9f17c78a6
Matt Jones <bigboxer23@gmail.com>
2024-10-15 09:59:24 -0500

    chore: bump springboot version

--- src/main/java/com/bigboxer23/lights/controllers/aggregate/FanSystemController.java
+++ src/main/java/com/bigboxer23/lights/controllers/aggregate/FanSystemController.java
@@ -42,7 +42,7 @@ public class FanSystemController {
 		logger.info("FanSystemController initialized and enabled: " + !disabled.get());
 	}
 
-	@Scheduled(cron = "0 0 */6 * * *") // every 6 hours
+	@Scheduled(cron = "0 0 */4 * * *") // every 6 hours
 	public void runFans() throws IOException, InterruptedException {
 		if (disabled.get()) {
 			return;
