a0b1bc42d52124e94503631abed18c9ca6b34c97
Matt Jones <bigboxer23@gmail.com>
2025-02-01 14:02:44 -0600

    chore: spotless

--- src/main/java/com/bigboxer23/lights/controllers/scene/DaylightController.java
+++ src/main/java/com/bigboxer23/lights/controllers/scene/DaylightController.java
@@ -10,11 +10,8 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.io.IOException;
 import java.net.HttpURLConnection;
-
 import lombok.extern.slf4j.Slf4j;
 import okhttp3.Response;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
