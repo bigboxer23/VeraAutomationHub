3e32707dd67bd28123180cca5b971238677e1b93
Matt Jones <bigboxer23@gmail.com>
2025-02-01 14:05:18 -0600

    chore: spotless (#155)

--- src/main/java/com/bigboxer23/lights/servlets/TokenDistributionServlet.java
+++ src/main/java/com/bigboxer23/lights/servlets/TokenDistributionServlet.java
@@ -10,10 +10,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.net.HttpURLConnection;
-
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.http.MediaType;
